b02b9ad66e498c2ac410066c12f79279228e59b4
Strawbang <dbougouffa@gmail.com>
2023-10-27 07:24:26 +0200

    try to setup

---
+++ Domain/User.cs
@@ -0,0 +1,18 @@
+﻿namespace voicevista_back.Domain
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        public User(int userId, string username, string password, string email)
+        {
+            UserId = userId;
+            Username = username;
+            Password = password;
+            Email = email;
+        }
+    }
+}
