e5345638d06110dce0568f1802cbd2949721748f
Strawbang <dbougouffa@gmail.com>
2023-10-31 19:29:52 +0100

    add example setup

---
+++ Application/User/UserRegisterService.cs
@@ -0,0 +1,17 @@
+﻿namespace voicevista_back.Application.User
+{
+    public class UserRegisterService : IUserRegisterService
+    {
+        private static List<Domain.User> _users = new List<Domain.User>();
+        public bool UserRegister(CreateRequestUser createRequestUser)
+        {
+            if (_users.Any(user => user.Username == createRequestUser.Username))
+            {
+                return false;
+            }
+
+            _users.Add(createRequestUser.ToDomain());
+            return true;
+        }
+    }
+}
